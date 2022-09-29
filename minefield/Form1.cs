b1b6aa7416aa960cde1d5dbb09475b4fcb1b9659
Kaan YILDIRIM <yldrm.gs.55@gmail.com>
2022-09-29 21:29:30 +0300

    Form created

--- minefield/Form1.cs
+++ minefield/Form1.cs
@@ -16,5 +16,11 @@ namespace minefield
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form easyForm = new Form();
+            easyForm.Show();
+        }
     }
 }
