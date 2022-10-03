0f297c5d9f00f2e61ea8e4bc83faeac49953eb93
Kaan YILDIRIM <yldrm.gs.55@gmail.com>
2022-10-03 14:28:19 +0300

    2d array and mine location

--- minefield/Form1.cs
+++ minefield/Form1.cs
@@ -16,9 +16,47 @@ namespace minefield
         {
             InitializeComponent();
         }
+        private void createMine()
+        {
+            int[,] minefieldLoc =new int[13,13];
+            Random rnd = new Random();
+            int[,] mineLoc = new int[2, 7];
 
+            for (int i = 0; i < 13; i++)
+            {
+                for (int x = 0; x < 13; x++)
+                {
+                    minefieldLoc[i,x] = 0 ;
+                    
+                }
+            }
+            
+            for (int i = 0; i < 7; i++)
+            {
+                int height = rnd.Next(13);
+                int width = rnd.Next(13);
+                if (minefieldLoc[height,width]==9)
+                {
+                    i--;
+                }
+                else
+                {
+                    minefieldLoc[height, width] = 9;
+                    mineLoc[0, i] = height;
+                    mineLoc[1, i] = width;
+                    //Console.WriteLine(mineLoc[0, i].ToString());
+                    //Console.WriteLine(mineLoc[1, i].ToString());
+                }
+
+                //Console.WriteLine(i.ToString());
+
+            }
+            
+            
+        }
         private void diffLvl (object sender, EventArgs e)
         {
+            createMine();
             int wBtn = 25;
             Form easyForm = new Form();
             easyForm.Width = 363;
