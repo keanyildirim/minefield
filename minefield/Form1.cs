4bbba5b2adfb201514d06b3fe4a9e78391ce5f55
Kaan YILDIRIM <yldrm.gs.55@gmail.com>
2022-10-04 19:21:38 +0300

    diziye ulaşma ve butonların click olayları

--- minefield/Form1.cs
+++ minefield/Form1.cs
@@ -16,13 +16,51 @@ namespace minefield
         {
             InitializeComponent();
         }
+        private void createMine()
+        {
+            int[,] minefieldLoc =new int[13,13];
+            Random rnd = new Random();
+            int[,] mineLoc = new int[2, 7];
 
+            for (int i = 0; i < 13; i++)
+            {
+                for (int x = 0; x < 13; x++)
+                {
+                    minefieldLoc[i,x] = 0 ;
+                    
+                }
+            }
+            
+            for (int i = 0; i < 7; i++)
+            {
+                int height = rnd.Next(13);
+                int width = rnd.Next(13);
+                if (minefieldLoc[height,width]==9)
+                {
+                    i--;
+                }
+                else
+                {
+                    minefieldLoc[height, width] = 9;
+                    mineLoc[0, i] = height;
+                    mineLoc[1, i] = width;
+                    //Console.WriteLine(mineLoc[0, i].ToString());
+                    //Console.WriteLine(mineLoc[1, i].ToString());
+                }
+
+                //Console.WriteLine(i.ToString());
+
+            }
+            
+            
+        }
         private void diffLvl (object sender, EventArgs e)
         {
             int wBtn = 25;
             Form easyForm = new Form();
             easyForm.Width = 363;
             easyForm.Height = 383;
+            
             for (int i = 0; i < 13; i++)
             {
                 for (int x = 0; x < 13; x++)
@@ -31,11 +69,22 @@ namespace minefield
                     uknBtn.Location = new Point(10+i*wBtn,10+x*wBtn);
                     uknBtn.Width = wBtn;
                     uknBtn.Height = wBtn;
+                    uknBtn.Text = "*";
                     easyForm.Controls.Add(uknBtn);
-
+                    uknBtn.Click +=new EventHandler(clickUnkBtn);
                 }
             }
             easyForm.Show();
         }
+
+        private void clickUnkBtn(object sender, EventArgs e)
+        {
+            Console.WriteLine("hii");
+            //Button clickedUnkBtn = (Button)sender;
+            //clickedUnkBtn.Enabled = false;
+            Console.WriteLine(mineLoc[0, 0]);
+            
+        }
+
     }
 }
